267778bbe9e88b319b4747a4d0c1240132c356b3
samppa13 <samuelkosola@gmail.com>
2023-01-18 17:40:07 +0200

    3.16: puhelinluettelo ja tietokanta, step4

--- index.js
+++ index.js
@@ -55,11 +55,6 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-const generateId = () => {
-    const id = Math.round(Math.random() * 100000)
-    return id
-}
-
 app.post('/api/persons', (request, response) => {
     const body = request.body
     if (!body.name) {
@@ -81,12 +76,29 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 61580
 app.listen(PORT, () => {})

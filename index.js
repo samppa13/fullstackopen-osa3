570e0ea94f22e81f0da3ca449a4114ae5b5f27e3
samppa13 <samuelkosola@gmail.com>
2023-01-18 12:29:06 +0200

    bugin korjaus

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/api/persons', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    persons = persons.filter(person => persons.id !== id)
+    persons = persons.filter(person => person.id !== id)
     response.status(204).end()
 })
 
